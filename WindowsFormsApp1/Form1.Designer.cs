9fd4a47b4197e39e04a2c205662e68453be7b845
fatih <70587868+fatcan123@users.noreply.github.com>
2021-02-20 14:29:23 +0300

    ilk commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textbox1 = new System.Windows.Forms.TextBox();
+            this.textbox2 = new System.Windows.Forms.TextBox();
+            this.textbox3 = new System.Windows.Forms.TextBox();
+            this.textbox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyadı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Okul No:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Dogum Yılı:";
+            // 
+            // textbox1
+            // 
+            this.textbox1.Location = new System.Drawing.Point(105, 47);
+            this.textbox1.Name = "textbox1";
+            this.textbox1.Size = new System.Drawing.Size(100, 22);
+            this.textbox1.TabIndex = 5;
+            // 
+            // textbox2
+            // 
+            this.textbox2.Location = new System.Drawing.Point(105, 78);
+            this.textbox2.Name = "textbox2";
+            this.textbox2.Size = new System.Drawing.Size(100, 22);
+            this.textbox2.TabIndex = 6;
+            // 
+            // textbox3
+            // 
+            this.textbox3.Location = new System.Drawing.Point(105, 112);
+            this.textbox3.Name = "textbox3";
+            this.textbox3.Size = new System.Drawing.Size(100, 22);
+            this.textbox3.TabIndex = 7;
+            // 
+            // textbox4
+            // 
+            this.textbox4.Location = new System.Drawing.Point(105, 144);
+            this.textbox4.Name = "textbox4";
+            this.textbox4.Size = new System.Drawing.Size(100, 22);
+            this.textbox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(311, 78);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(510, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 29);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(510, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 40);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(190, 187);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(505, 179);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textbox4);
+            this.Controls.Add(this.textbox3);
+            this.Controls.Add(this.textbox2);
+            this.Controls.Add(this.textbox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textbox1;
+        private System.Windows.Forms.TextBox textbox2;
+        private System.Windows.Forms.TextBox textbox3;
+        private System.Windows.Forms.TextBox textbox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string deger1 = textbox4.Text;
+            //burada textbox4 deki değeri aldım 
+            int dogum_yili = Int32.Parse(deger1);
+            //string i int e dönüştürdüm.
+            int yas = 0;
+            for (yas = 0; yas + dogum_yili < 2020; yas++) ;
+            this.textBox5.Text = yas.ToString();
+            //bu döngüyü de çıkarma işlemi yapmayın dediğiniz için yazdım 2020 ye gelinceye kadar yas a ekledi 
+            //bir altında ki satırda da çıkan sonucu textbox5 e yazdırdı.
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string adsoyad = "";
+            adsoyad = textbox1.Text + " " +textbox2.Text;
+            //burada isim soyisim birleşik yazmak için bir string e atadım adsoyad adında 
+            string harf1 = textBox5.Text.Substring(0, 1);
+            //burada ilk harfi aldım 
+            int rakam1 = Int32.Parse(harf1);
+            //int e çevirdim
+            string harf2 = textBox5.Text.Substring(1, 1);
+            //2. harfi aldım 
+            int rakam2 = Int32.Parse(harf2);
+            //int e çevirdim
+            int toplam = rakam1 + rakam2;
+            //toplam adında bir int e ilk 2 rakamı ekledim
+            for(int i = 1; i <= toplam; i++)
+            {
+                listBox1.Items.Add(adsoyad);
+            }
+            //son olarak for döngüsünde toplam a kadar ad soyad ekledim 
+            
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sayi1 = textbox3.Text;
+            //textbox3 deki değeri sayi1 değişkenine atadım.
+            string ilkdort = sayi1.Substring(0, 4);
+            //sayi1 deki ilk 4 rakamı ilkdort değişkenine atadım.
+            int ilkdortlu = Int32.Parse(ilkdort);
+            //sonra bunu int e çevirdim.
+            string sondort = sayi1.Substring(5, 4);
+            //sayi1 deki son 4 rakamı sondort değişkenine atadım.
+            int sondortlu = Int32.Parse(sondort);
+            //sonra int e çevirdik.
+            int kucuk, buyuk = 0;
+            //kucuk ve buyuk diye 2 int değişken tanımladım.
+            
+            if (ilkdortlu < sondortlu)
+            {
+                kucuk = ilkdortlu;
+                buyuk = sondortlu;
+            }
+            else  {
+                kucuk = sondortlu;
+                buyuk = ilkdortlu;
+            }
+            //if koşuluna sokup ilkdortlu nün mü sondortlu nün mü büyük olduğunu buldum.
+            
+            int kalan1 = kucuk % 100;
+            //bu kısımda kucuk sayının 100 e bölümünden kalanı buldum.
+            
+            int ilksayi = kucuk + 100 - kalan1;
+            // aralıkda ki ilk 100 e tam bölünen sayıyı buldum.
+
+            string satir = "";
+            //satir adında bir değişken tanımladım.
+            for(int i = ilksayi; i < buyuk; i = i + 100)
+            {
+                
+                if (ilksayi % 1000 == i % 1000 && i!=ilksayi)
+                {
+                    satir = satir + Environment.NewLine;
+
+                }
+                
+                satir = satir + " " + i.ToString();
+               
+            }
+            //for döngüsünde ilk sayi buyuk sayıya gelinceye kadar 100 er 100 er ekleme yaptım.
+            //içinde ki if koşulunda her 10 satırda bir alt satıra geçmesini sağladım.
+            //&& bu koşulla da ilk başta eşitken altsatıra geçmemesi için yazdım.
+         
+            richTextBox1.Text = satir;
+            //son olarak da richtextbox a yazdırdım.
+        }
+
+    }
+}
